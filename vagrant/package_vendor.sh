84281c4542274f4ec2d2f29a12294e400c6d6249
Andrew Choi <andrew@choi.com>
2015-03-23 13:46:12 -0400

    Found locations of libgfortran in 14.04 install--hiding in a subdir

--- vagrant/package_vendor.sh
+++ vagrant/package_vendor.sh
@@ -5,8 +5,8 @@ mkdir -p vendor/lib
 ssh-keygen -f ~/.ssh/known_hosts -R [localhost]:2222
 # ssh-keyscan -H localhost:2222 >> ~/.ssh/known_hosts
 scp -o StrictHostKeyChecking=no -i .vagrant/machines/default/virtualbox/private_key -r -P 2222 vagrant@localhost:/usr/lib/atlas-base vendor/lib
-# scp -i .vagrant/machines/default/virtualbox/private_key -r -P 2222 vagrant@localhost:/usr/lib/libgfortran.so.3 vendor/lib
-# scp -i .vagrant/machines/default/virtualbox/private_key -r -P 2222 vagrant@localhost:/usr/lib/libgfortran.so.3.0.0 vendor/lib
+scp -i .vagrant/machines/default/virtualbox/private_key -r -P 2222 vagrant@localhost:/usr/lib/x86_64-linux-gnu/libgfortran.so.3 vendor/lib
+scp -i .vagrant/machines/default/virtualbox/private_key -r -P 2222 vagrant@localhost:/usr/lib/x86_64-linux-gnu/libgfortran.so.3.0.0 vendor/lib
 tar -cvzf npscipy.tar.gz vendor
 mv npscipy.tar.gz ..
 rm -rf vendor

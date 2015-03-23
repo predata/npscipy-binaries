9e7080f31f9c89ff141996765ac08bdfa347c81b
Andrew Choi <andrew@choi.com>
2015-03-23 13:26:08 -0400

    Update packaging scripts

--- vagrant/package_vendor.sh
+++ vagrant/package_vendor.sh
@@ -2,7 +2,7 @@
 
 rm -rf vendor
 mkdir -p vendor/lib
-# ssh-keygen -f ~/.ssh/known_hosts -R [localhost]:2222
+ssh-keygen -f ~/.ssh/known_hosts -R [localhost]:2222
 # ssh-keyscan -H localhost:2222 >> ~/.ssh/known_hosts
 scp -o StrictHostKeyChecking=no -i .vagrant/machines/default/virtualbox/private_key -r -P 2222 vagrant@localhost:/usr/lib/atlas-base vendor/lib
 # scp -i .vagrant/machines/default/virtualbox/private_key -r -P 2222 vagrant@localhost:/usr/lib/libgfortran.so.3 vendor/lib
---
+++ vagrant/repackage.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+source package_numpy.sh && source package_scipy.sh && source package_vendor.sh 
